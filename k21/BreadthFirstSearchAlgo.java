038d31000b972931b4bff52744fbe0d036c9996d
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-10-16 22:33:57 +0700

    up final lab

--- k21/BreadthFirstSearchAlgo.java
+++ k21/BreadthFirstSearchAlgo.java
@@ -1,2 +1,65 @@
-package k21;public class BreadthFirstSearchAlgo {
+package k21;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BreadthFirstSearchAlgo implements ISearchAlgo {
+
+
+    @Override
+    public Node execute(Node root, String goal) {
+        Queue<Node> frontier = new LinkedList<Node>();
+        Node goalNode = new Node(goal);
+        frontier.add(root);
+        List<Node> explorer = new ArrayList<>();
+        while (!frontier.isEmpty()) {
+            Node chooseNode = frontier.poll();
+            explorer.add(chooseNode);
+            if (chooseNode.equals(goalNode)) {
+                return chooseNode;
+            }
+            for (Node node : chooseNode.getChildrenNodes()) {
+                if (!frontier.contains(node) && !explorer.contains(node)) {
+                    node.setParent(chooseNode);
+                    node.setPathCost(chooseNode.getPathCost() + chooseNode.getEdge(node).getWeight());
+                    frontier.add(node);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        Queue<Node> frontier = new LinkedList<Node>();
+        frontier.add(root);
+        Node startNode = new Node(start);
+        boolean flag = false;
+        List<Node> explorer = new ArrayList<>();
+        while (!frontier.isEmpty()) {
+            Node chooseNode = frontier.poll();
+            explorer.add(chooseNode);
+            if (chooseNode.getLabel().equals(start)) {
+                frontier.clear();
+                explorer.clear();
+                flag = true;
+            }
+            if (chooseNode.getLabel().equals(goal) && flag) {
+                return chooseNode;
+            }
+
+            for (Node node : chooseNode.getChildrenNodes()) {
+                if (!frontier.contains(node) && !explorer.contains(node)) {
+                    if (flag) {
+                        node.setParent(chooseNode);
+                        node.setPathCost(chooseNode.getPathCost() + chooseNode.getEdge(node).getWeight());
+                    }
+                    frontier.add(node);
+                }
+            }
+        }
+        return null;
+    }
 }
