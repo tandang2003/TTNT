0c5f30605d0eb6bca98d07a219aa5eb3331273c0
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-10-16 22:32:46 +0700

    up final lab

---
+++ k21/DepthLimitedSearch.java
@@ -0,0 +1,8 @@
+package k21;
+
+public class DepthLimitedearch {
+    public Node execute(Node root, String goal, int limitedDepth){
+        
+    }
+
+}
---
+++ k21/UniformCostSearchAlgo.java
@@ -0,0 +1,73 @@
+package k21;
+
+import java.util.*;
+
+public class TreeUniformCostSearchAlgo implements ISearchAlgo {
+    @Override
+    public Node execute(Node root, String goal) {
+        PriorityQueue<Node> frontier = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                return (int) (node1.getPathCost() - node2.getPathCost());
+            }
+        });
+        frontier.offer(root);
+        List<Node> explorer = new ArrayList<>();
+        while (!frontier.isEmpty()) {
+            Node chosen = frontier.poll();
+            if (chosen.getLabel().equals(goal)) {
+                return chosen;
+            }
+            explorer.add(chosen);
+            for (Edge edge : chosen.getChildren()) {
+                Node child = edge.getEnd();
+                if (!explorer.contains(child) && !frontier.contains(child)) {
+                    child.setParent(chosen);
+                    child.setPathCost(chosen.getPathCost() + edge.getWeight());
+                    frontier.offer(child);
+                } else if (child.getPathCost() > chosen.getPathCost() + edge.getWeight()) {
+                    child.setParent(chosen);
+                    child.setPathCost(chosen.getPathCost() + edge.getWeight());
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        PriorityQueue<Node> frontier = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                return (int) (node1.getPathCost() - node2.getPathCost());
+            }
+        });
+        boolean flag = false;
+        frontier.offer(root);
+        List<Node> explorer = new ArrayList<>();
+        while (!frontier.isEmpty()) {
+            Node chosen = frontier.poll();
+            if (chosen.getLabel().equals(goal) && flag) {
+                return chosen;
+            }
+            if (chosen.getLabel().equals(start)) {
+                frontier.clear();
+                flag = true;
+            }
+            explorer.add(chosen);
+            for (Edge edge : chosen.getChildren()) {
+                Node child = edge.getEnd();
+                if (!explorer.contains(child) && !frontier.contains(child)) {
+                    if(flag) {
+                        child.setPathCost(chosen.getPathCost() + edge.getWeight());
+                        child.setParent(chosen);
+                    }frontier.offer(child);
+                } else if (child.getPathCost() > chosen.getPathCost() + edge.getWeight()) {
+                    child.setParent(chosen);
+                    child.setPathCost(chosen.getPathCost() + edge.getWeight());
+                }
+            }
+        }
+        return null;
+    }
+}
