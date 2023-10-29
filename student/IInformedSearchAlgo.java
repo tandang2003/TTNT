f216a75cefe0103e0dca4a1b4c401136677d7ea8
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-10-30 00:50:20 +0700

    update lab 4

---
+++ student/IInformedSearchAlgo.java
@@ -0,0 +1,7 @@
+package lab4.inform.student;
+
+public interface IInformedSearchAlgo {
+	public Node execute(Node root, String goal);
+
+	public Node execute(Node root, String start, String goal);
+}
