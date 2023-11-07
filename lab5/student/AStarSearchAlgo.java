ea42b54dcb2c02eed75c696341f776c49e43de44
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-07 11:36:07 +0700

    update late lab 5

--- lab5/student/AStarSearchAlgo.java
+++ lab5/student/AStarSearchAlgo.java
@@ -1,2 +1,35 @@
-package puzzle_8.student;public class AStarSearchAlgo {
+package puzzle_8.student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class AStarSearchAlgo implements IPuzzleAlgo{
+    @Override
+    public Node execute(Puzzle model) {
+        PriorityQueue<Node> frontier = new PriorityQueue<>(PuzzleUtils.HeuristicComparatorByF);
+        List<Node> explored = new ArrayList<>();
+        Node init = model.getInitialState();
+        frontier.add(init);
+        while (!frontier.isEmpty()) {
+            Node chosen = frontier.poll();
+            if (model.computeH2(chosen) == 0) return chosen;
+            else {
+                explored.add(chosen);
+                List<Node> children= model.getSuccessors(chosen);
+                for(Node child: children){
+                    if(!frontier.contains(child)&& !explored.contains(child)){
+                        child.setG(chosen.getG()+1);
+                        frontier.add(child);
+                    }else{
+                        if(frontier.contains(child) && chosen.getF()<child.getF()){
+                            child.setG(chosen.getG()+1);
+                            frontier.add(child);
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
--- lab5/student/GreedyBestFirstSearch.java
+++ lab5/student/GreedyBestFirstSearch.java
@@ -1,2 +1,31 @@
-package puzzle_8.student;public class GreedyBestFirstSearch {
+package puzzle_8.student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import static puzzle_8.student.Puzzle.operators;
+
+public class  GreedyBestFirstSearch implements IPuzzleAlgo {
+    @Override
+    public Node execute(Puzzle model) {
+        PriorityQueue<Node> frontier = new PriorityQueue<>(PuzzleUtils.HeuristicComparatorByH);
+        List<Node> explored = new ArrayList<>();
+        Node init = model.getInitialState();
+        frontier.add(init);
+        while (!frontier.isEmpty()) {
+            Node chosen = frontier.poll();
+            if (model.computeH2(chosen) == 0) return chosen;
+            else {
+                explored.add(chosen);
+                List<Node> children= model.getSuccessors(chosen);
+                for(Node child: children){
+                    if(!frontier.contains(child)&& !explored.contains(child)){
+                        frontier.add(child);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
